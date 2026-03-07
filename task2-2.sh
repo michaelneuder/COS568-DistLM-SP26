0a1985ee36eb293d1c42499299ddc1fbba521958
Michael Neuder <michael.neuder@gmail.com>
2026-03-07 00:06:05 -0500

    epochs 1

--- task2-2.sh
+++ task2-2.sh
@@ -11,7 +11,7 @@ python3 run_glue.py \
   --max_seq_length 128 \
   --per_device_train_batch_size 16 \
   --learning_rate 2e-5 \
-  --num_train_epochs 3 \
+  --num_train_epochs 1 \
   --output_dir /tmp/$TASK_NAME/ \
   --overwrite_output_dir \
   --master_ip  10.10.1.2 \
